0d947e7cb5a5f884929fc87790bf3c67cc3068e8
Aramxxx8691 <khurshudyan2013@gmail.com>
2022-11-30 19:00:50 +0400

    C_1

---
+++ microshell1.c
@@ -0,0 +1,67 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int fd;
+
+int	err(char *str)
+{
+	while (*str)
+		write(2, str++, 1);
+	return(1);
+}
+
+int cdir(char **av, int i)
+{
+	if (i != 2)
+		return err("error: cd: bad arguments\n");
+	if (chdir(av[i]))
+		return err("error: cd: cannot change directory to ") & err(av[i]) & err("\n");
+	return (0);
+}
+
+int	ex(char **av, char **envp, int i)
+{
+	int	fds[2];
+	int	res;
+	int pip = (av[i] && !strcmp(av[i], "|"));
+
+	if (pip && (pipe(fds)))
+		return err("error: fatal\n");
+	int pid = fork();
+	if (!pid)
+	{
+		av[i] = 0;
+		if (dup2(fd, 0) == -1 | close(fd) == -1 | (pip && (dup2(fds[1], 1)
+			== -1 | close(fds[0]) == -1 | close(fds[1]) == -1)))
+			return err("error: fatal\n");
+		execve(*av, av, envp);
+		return err("error: cannot execute ") & err(*av) & err("\n");
+	}
+	if ((pip && (dup2(fds[0], fd) == -1 | close(fds[0]) == -1
+		| close(fds[1]) == -1)) | (!pip && dup2(0, fd) == -1) |
+		waitpid(pid, &res, 0) == -1)
+		return err("error: fatal\n");
+	return WIFEXITED(res) && WEXITSTATUS(res);
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	(void)ac;
+	int	i = 0;
+	int	j = 0;
+
+	fd = dup(0);
+	while (av[i] && av[++i])
+	{
+		av = av + i;
+		i = 0;
+		while (av[i] && strcmp(av[i] , "|") && strcmp(av[i] , ";"))
+			i++;
+		if (!strcmp(*av, "cd"))
+			j = cdir(av, i);
+		else if (i)
+			j = ex(av, envp, i);
+	}
+	return((dup2(0, fd) == -1) && err("error: fatal\n")) | j;
+}
---
+++ microshell2.c
@@ -0,0 +1,89 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int	err(char *str, char *av)
+{
+	while (*str)
+		write(2, str++, 1);
+	if (av)
+		while (*av)
+			write(2, av++, 1);
+	write(2, "\n", 1);
+	return (1);
+}
+
+int cdir(char **av, int i)
+{
+	if (i != 2)
+		return err("error: cd: bad arguments", NULL);
+	if (chdir(av[i]) != 0)
+	{
+		printf("[%d]\n", i);
+		return err("error: cd: cannot change directory to ", av[i] );
+	}
+	return (0);
+}
+
+int	ex(char **av, int i, int fd, char **envp)
+{
+	av[i] = NULL;
+	dup2(fd, 0);
+	close(fd);
+	execve(av[0], av, envp);
+	return (err("error: cannot execute ", av[0]));
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	(void)ac;
+	int	i = 0;
+	int	fds[2];
+	int	fd = dup(0);
+
+	while (av[i] && av[i + 1])
+	{
+		av = &av[i + 1];
+		i = 0;
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
+			i++;
+		if (strcmp(av[0], "cd") == 0)
+			cdir(av, i);
+		else if (i != 0 && (av[i] == NULL || strcmp(av[i], ";") == 0))
+		{
+			if (fork() == 0)
+			{
+				if (ex(av, i, fd, envp))
+					return (1);
+			}
+			else
+			{
+				close(fd);
+				while (waitpid(-1, NULL, WUNTRACED) != -1)
+					;
+				fd = dup(0);
+			}
+		}
+		else if (i != 0 && strcmp(av[i], "|") == 0)
+		{
+			pipe(fds);
+			if (fork() == 0)
+			{
+				dup2(fds[1], 1);
+				close(fds[0]);
+				close(fds[1]);
+				if (ex(av, i, fd, envp))
+					return (1);
+			}
+			else
+			{
+				close(fds[1]);
+				close(fd);
+				fd = fds[0];
+			}
+		}
+	}
+	close(fd);
+	return (0);
+}
