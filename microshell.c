95f0eedf8d191e23fb20246426a20beba7648ea9
Aramxxx8691 <khurshudyan2013@gmail.com>
2022-12-09 20:40:25 +0400

    C_3

--- microshell.c
+++ microshell.c
@@ -4,11 +4,11 @@
 
 int fd;
 
-int err(char *s)
+int err(char *str)
 {
-	while (*s)
-		write(2, s++, 1);
-	return 1;
+	while (*str)
+		write(2, str++, 1);
+	return (1);
 }
 
 int cdir(char **av, int i)
@@ -17,30 +17,27 @@ int cdir(char **av, int i)
 		return err("error: cd: bad arguments\n");
 	if (chdir(av[1]))
 		return err("error: cd: cannot change directory to ") & err(av[1]) & err("\n");
-	return 0;
+	return (0);
 }
 
 int ex(char **av, char **envp, int i)
 {
+	int res;
 	int fds[2];
-	int	res;
 	int pip = (av[i] && !strcmp(av[i], "|"));
-	
+
 	if (pip && (pipe(fds)))
 		return err("error: fatal\n");
 	int pid = fork();
 	if (!pid)
 	{
 		av[i] = 0;
-		if (dup2(fd, 0) == -1 | close(fd) == -1 | (pip && (dup2(fds[1], 1)
-			== -1 | close(fds[0]) == -1 | close(fds[1]) == -1)))
+		if (dup2(fd, 0) == -1 | close(fd) == -1 | (pip && (dup2(fds[1], 1) == -1 | close(fds[0]) == -1 | close(fds[1]) == -1)))
 			return err("error: fatal\n");
 		execve(*av, av, envp);
 		return err("error: cannot execute ") & err(*av) & err("\n");
 	}
-	if ((pip && (dup2(fds[0], fd) == -1 | close(fds[0]) == -1
-		| close(fds[1]) == -1)) | (!pip && dup2(0, fd) == -1) |
-		waitpid(pid, &res, 0) == -1)
+	if ((pip && (dup2(fds[0], fd) == -1 | close(fds[0]) == -1 | close(fds[1]) == -1)) | (!pip && dup2(0, fd) == -1) | waitpid(pid, &res, 0) == -1)
 		return err("error: fatal\n");
 	return WIFEXITED(res) && WEXITSTATUS(res);
 }
@@ -52,16 +49,16 @@ int main(int ac, char **av, char **envp)
 	int j = 0;
 
 	fd = dup(0);
-	while (av[i] && av[++i])
+	while(av[i] && av[++i])
 	{
 		av = av + i;
 		i = 0;
-		while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
 			i++;
 		if (!strcmp(*av, "cd"))
 			j = cdir(av, i);
 		else if (i)
 			j = ex(av, envp, i);
 	}
-	return ((dup2(0, fd) == -1) && err("error: fatal\n")) | j;
+	return (dup2(0, fd) == -1 && err("error: fatal\n")) | j;
 }
